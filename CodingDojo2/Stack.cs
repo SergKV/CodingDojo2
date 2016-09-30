6c8440001c634358891fe2376efad6cb4c7592b1
user <user@USER-PC>
2016-09-30 22:41:54 +0200

    Add project files.

---
+++ CodingDojo2/Stack.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodingDojo2
+{
+    public class Stack<T>
+    {
+        public class Node
+        {
+
+            private T _data;
+            private Node _nextNode;
+
+            public Node(T t)
+            {
+                _nextNode = null;
+                _data = t;
+            }
+
+            public Node NextNode
+            {
+                get { return _nextNode; }
+                set { _nextNode = value; }
+            }
+
+            public T Value
+            {
+                get { return _data; }
+                set { _data = value; }
+            }
+        }
+
+        private Node head = null;
+
+        public Stack()
+        {
+            head = null;
+        }
+
+        public void Push(T t)
+        {
+            Node x = new Node(t);
+            if (head != null)
+            {
+                x.NextNode = head;
+            }
+            head = x;
+        }
+
+        public T Pop()
+        {
+            Node temp = head;
+            head = head.NextNode;
+            temp.NextNode = null;
+            return temp.Value;
+        }
+
+
+        public T Peek()
+        {
+            if (head.Value != null)
+            {
+                return head.Value;
+            }
+            return head.Value;
+        }
+    }
+}
---
+++ CodingDojo2/TestObject.cs
@@ -0,0 +1,19 @@
+﻿namespace CodingDojo2
+{
+    internal class TestObject
+    {
+        private int v1;
+        private string v2;
+
+        public TestObject(int v1, string v2)
+        {
+            this.v1 = v1;
+            this.v2 = v2;
+        }
+
+        public override string ToString()
+        {
+            return string.Format("{0}, {1}", v1, v2);
+        }
+    }
+}
